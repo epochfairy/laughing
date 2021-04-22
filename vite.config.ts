ac328131561d3041bc77651e4c57d9b9816b227a
root <root@localhost.localdomain>
2021-04-23 00:59:59 +0800

    js

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base:'/laughing/'   // 对应GitHub项目名称
 })
