d42775737e5fc785b16d3194a461c98fec481f9a
epochfairy <epochfairy@users.noreply.github.com>
2021-04-28 07:56:27 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 81ef9622de3e667c914f34b8b816e09fbb6299ee

---
+++ assets/Logo.e7204910.js
@@ -0,0 +1,1 @@
+import{p as s,l as a,o,c,b as t,t as e,i as l}from"./vendor.2ae82722.js";const n={},d=l();s("data-v-7755cfcb");const r={class:"login-logo"};a();const p=d(((s,a)=>(o(),c("div",r,[t("span",null,e(s.$t("message.logo")),1)]))));n.render=p,n.__scopeId="data-v-7755cfcb";export default n;
--- assets/User.0c3f73df.js
+++
@@ -1,1 +0,0 @@
-import{o as r,c as n}from"./vendor.326c1501.js";const o={};o.render=function(o,e){return r(),n("h1",null,"用户")};export default o;
