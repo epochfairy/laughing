9446c7c5d5a3b6e50808b6b0e31a2764608e18b7
epochfairy <epochfairy@users.noreply.github.com>
2021-05-05 07:07:43 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 8a2b0ee97331803b02b890ba295ac831ff9899eb

---
+++ assets/SlotParent.5135e14f.js
@@ -0,0 +1,1 @@
+import{d as r,r as e,o as a,c as o}from"./vendor.b5a5699a.js";var t=r({setup(){}});t.render=function(r,t,s,n,u,d){const c=e("router-view");return a(),o(c)};export default t;
--- assets/User.5ad217ef.js
+++
@@ -1,1 +0,0 @@
-import{o as r,c as e}from"./vendor.2ae82722.js";const n={};n.render=function(n,o){return r(),e("h1",null,"用户")};export default n;
